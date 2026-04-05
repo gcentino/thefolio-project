c0a993f01f5c8d1a7ac221d968a64ec59a273cfc
Greshamin Centino <gcentino23109603@student.dmmmsu.edu.ph>
2026-04-05 23:04:04 +0800

    temporary allow all cors

--- backend/server.js
+++ backend/server.js
@@ -25,15 +25,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,
   credentials: true
 }));
 
